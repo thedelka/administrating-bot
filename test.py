fc5b735ac48265a459be845253464755054726fa
thedelka <alishersurkhiev@gmail.com>
2025-05-01 14:27:01 +0500

    3

--- test.py
+++
@@ -1,8 +0,0 @@
-import configparser
-import json
-from Settings.get_config import get_config
-
-menu_buttons_texts = json.loads(get_config("HELP_MENU_SETTINGS", "buttons"))
-print(menu_buttons_texts.keys())
-
-
